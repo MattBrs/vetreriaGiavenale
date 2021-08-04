eca30f9e81b6a39400b9ac2638417f0448f82236
MattBrs <matteo.brusarosc@gmail.com>
2021-08-04 11:55:04 +0200

    services page added

--- src/app/servizi/services.component.ts
+++ src/app/servizi/services.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ServicesFetchService} from "./servicesFetch.service";
 import {Service} from "./service.model";
 import {Subscription} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-services',
@@ -14,8 +15,21 @@ export class ServicesComponent implements OnInit, OnDestroy {
   constructor(private fetchService: ServicesFetchService) { }
 
   ngOnInit(): void {
+    let service: {
+      name: string,
+      description: string
+    }[] = JSON.parse(<string>localStorage.getItem('services'));
+    if(service != null){
+      this.services = service;
+      console.log('services exist');
+    }else{
+      console.log('services null');
+    }
+
+
     this.serviceSub = this.fetchService.fetchData().subscribe( response => {
       this.services = response;
+      localStorage.setItem('services', JSON.stringify(this.services));
     });
   }
   ngOnDestroy() {
